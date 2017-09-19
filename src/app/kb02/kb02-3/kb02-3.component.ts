75d9e3129661e9646691c55c26391c69bc65bf62
Sontaya W <sontaya.wichakot@ktbcs.co.th>
2017-09-19 17:09:08 +0700

    แก้ไข css Angular Material ให้รันธีมแบบ Offline, แก้ไขแถบเมนูค้นหา

--- src/app/kb02/kb02-3/kb02-3.component.ts
+++ src/app/kb02/kb02-3/kb02-3.component.ts
@@ -1,9 +1,12 @@
 ﻿import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuTopComponent } from 'app/menu-top/menu-top.component';
 
 @Component({
   selector: 'kb02-3',
   templateUrl: './kb02-3.component.html',
-  styleUrls: ['./kb02-3.component.css']
+  styleUrls: ['./kb02-3.component.css'],
+  providers: [MenuTopComponent]
 })
 export class Kb023Component implements OnInit {
 
@@ -14,13 +17,23 @@ export class Kb023Component implements OnInit {
   ];
 
   SELECTED_DATE = this.dateList[0].id;
+  RADIO_TYPE = 'web';
 
-  constructor() {}
+  constructor(private route: Router, private MenuTop: MenuTopComponent) {
+    // TEST
+    // console.log('Route:' + route.url);
+    this.MenuTop.onClick(route.url);
+  }
+
+  // TEST
   onSearch(tab) {
     console.log(tab);
+    console.log(this.RADIO_TYPE);
   }
 
   ngOnInit() {
+    this.MenuTop.onClick(this.route.url); // TEST
+    this.MenuTop.topMenu_s = 'fontMenuTop'; // TEST
   }
 
 }
--- src/app/menu-top/menu-top.component.ts
+++ src/app/menu-top/menu-top.component.ts
@@ -1,4 +1,5 @@
 ﻿import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'menu-top',
@@ -7,9 +8,29 @@
 })
 export class MenuTopComponent implements OnInit {
 
-  constructor() {}
+  topMenu_c = 'fontMenuTop';
+  topMenu_s = 'Subcategory';
+  currentUrl: string;
+  constructor(private route: Router) {
+    console.log('Route:' + route.url);
+    console.log(this.topMenu_s);
+  }
+
+  onClick(index) {
+    if (index === '/kb021') {
+      this.topMenu_c = 'fontMenuTop';
+      this.topMenu_s = 'Subcategory';
+      console.log(index);
+    } else if (index === '/Kb023') {
+      this.topMenu_c = 'Subcategory';
+      this.topMenu_s = 'fontMenuTop';
+      console.log(index);
+    }
+    console.log(this.route.url);
+  }
 
   ngOnInit() {
+    console.log('On:' + this.route.url);
   }
 
 }
