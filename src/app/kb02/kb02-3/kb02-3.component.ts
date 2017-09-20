7e7c7aab3be0fd622146d2fbcb41b66b9c6614ce
Sontaya W <sontaya.wichakot@ktbcs.co.th>
2017-09-20 17:25:23 +0700

    แก้ไขบัค Disable Input Text, เปลี่ยน Radio Button ให้เหมือนเดิม

--- src/app/kb02/kb02-3/kb02-3.component.ts
+++ src/app/kb02/kb02-3/kb02-3.component.ts
@@ -1,9 +1,11 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MdDialog, MdDialogRef, MdDialogConfig } from '@angular/material';
+import { DialogSearchComponent } from 'app/controls/dialog-search/dialog-search.component';
 import { MenuTopComponent } from 'app/menu-top/menu-top.component';
 
 @Component({
-  selector: 'kb02-3',
+  selector: 'app-kb02-3',
   templateUrl: './kb02-3.component.html',
   styleUrls: ['./kb02-3.component.css'],
   providers: [MenuTopComponent]
@@ -19,10 +21,41 @@ export class Kb023Component implements OnInit {
   SELECTED_DATE = this.dateList[0].id;
   RADIO_TYPE = 'web';
 
-  constructor(private route: Router, private MenuTop: MenuTopComponent) {
+  // Disable & Show
+  DSTBALL;
+  SHOWBT;
+
+  // Input Text
+  TBBUKRS = '2109'; // รหัสงบประมาณ
+  TBZZPMT = '2100900006'; // รหัสหน่วยเบิกจ่าย
+
+  // Label
+  LBBUKRS = 'กรมอนามัย'; // TEST รหัสงบประมาณ
+  LBZZPMT = 'กองคลัง'; // TEST รหัสหน่วยเบิกจ่าย
+
+  // Date
+  T_DATE = new Date();
+  F_DATE = new Date();
+
+  // User ID
+  USERID;
+
+  constructor(private route: Router, private MenuTop: MenuTopComponent, public dialog: MdDialog,) {
     // TEST
     // console.log('Route:' + route.url);
     this.MenuTop.onClick(route.url);
+  }
+
+  // Open Dialog Search
+  dialogSearch(textcode, textname) {
+    const codetx = textcode;
+    const nametx = document.getElementById(textname).textContent;
+    console.log('Open: ' + codetx + ' | ' + nametx);
+    const config = new MdDialogConfig();
+    const dialogRef: MdDialogRef<DialogSearchComponent> = this.dialog.open(DialogSearchComponent, {panelClass: 'myapp-no-padding-dialog'});
+    dialogRef.componentInstance.code_t = codetx;
+    dialogRef.componentInstance.name_t = nametx;
+
   }
 
   // TEST
@@ -30,6 +63,7 @@ export class Kb023Component implements OnInit {
     console.log(tab);
     console.log(this.RADIO_TYPE);
   }
+
 
   ngOnInit() {
     this.MenuTop.onClick(this.route.url); // TEST
