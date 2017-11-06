7c68202bb0114fe3042ef718f248b970557c5806
Sontaya W <sontaya.wichakot@ktbcs.co.th>
2017-11-06 10:17:25 +0700

    Update Log Report

--- src/app/log-report/log-report.component.ts
+++ src/app/log-report/log-report.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { Http, Response } from '@angular/http';
-import { kb02 } from '../../models/kb02'; //import model เข้ามาใช้งาน
+import { kb02 } from '../../models/kb02'; // import model เข้ามาใช้งาน
 import { Observable } from 'rxjs';
 import 'rxjs/Rx';
 import 'rxjs/add/operator/filter';
@@ -18,8 +18,12 @@ export class LogReportComponent implements OnInit {
   kb02: kb02[];
   errorMessage: String;
   docid;
-  url = "http:///52.221.246.71:3000/api/getKb02Header/";
-  url2 = "http:///52.221.246.71:3000/api/kbo2LogReport/";
+  // TEST Online =>
+  url = 'http:///52.221.246.71:3000/api/getKb02Header/';
+  url2 = 'http:///52.221.246.71:3000/api/kbo2LogReport/';
+  // TEST local =>
+  // url = 'http:///localhost:3000/api/getKb02Header/';
+  // url2 = 'http:///localhost:3000/api/kbo2LogReport/';
   constructor(
     private http: Http,
     private route: ActivatedRoute,
@@ -27,17 +31,17 @@ export class LogReportComponent implements OnInit {
 
 
   getPosts(docid): Observable<kb02[]> {
-    return this.http.get(this.url+docid)
+    return this.http.get(this.url + docid)
       .map(this.extractData)
       .catch(this.handleErrorObservable);
   }
   getPosts2(docid): Observable<kb02[]> {
-    return this.http.get(this.url2+docid)
+    return this.http.get(this.url2 + docid)
       .map(this.extractData)
       .catch(this.handleErrorObservable);
   }
   private extractData(res: Response) {
-    let body = res.json();
+    const body = res.json();
     return Array.of(body);
   }
   private handleErrorObservable(error: Response | any) {
@@ -47,14 +51,14 @@ export class LogReportComponent implements OnInit {
   onGetDocSearch(years, docid) {
     this.getPosts(docid);
     this.getPosts2(docid);
-  };
+  }
 
   ngOnInit(): void {
     this.route.queryParams
       .filter(params => params.docid)
       .subscribe(params => {
         this.onGetDocSearch(params.years, params.docid); // Get Doc by Doc ID
-        this.docid=params.docid;
+        this.docid = params.docid;
       });
     this.observableBooks = this.getPosts( this.docid);
     this.observableBooks.subscribe(
