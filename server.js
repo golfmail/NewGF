ad6d5c0a2df38e7664205f007f18f438015a04f5
Sontaya W <tonklasw@icloud.com>
2017-11-27 16:03:36 +0700

    100, 600

--- server.js
+++ server.js
@@ -77,18 +77,27 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
-        res.status( err.code)
-        .json({
-            status: 'error',
-            message: err
-        });
+        if (err.code >= 100 && err.code < 600) {
+            res.status(err.code)
+            .json({
+                status: 'error',
+                message: err
+            });
+        } else {
+            res.status(500 )
+            .json({
+                status: 'error',
+                message: err
+            });
+        }
+        
     });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status) // 500
+    res.status(err.status || 500)
     .json({
         status: 'error',
         message: err.message
