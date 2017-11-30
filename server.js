643b05a714f52906985632fd26e91a5880ea6548
Sontaya W <tonklasw@icloud.com>
2017-11-30 10:16:37 +0700

    port

--- server.js
+++ server.js
@@ -1,24 +1,99 @@
-const express = require('express');
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var api = require('./api/index');
+
+var app = express();
 const path = require('path');
-const bodyParser = require('body-parser');
-const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+
+///////////////////////
+// Server Middleware
+///////////////////////
+
+app.use(logger(app.get("env") === "production" ? "combined" : "dev"));
+
+// parse application/json
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use('/api', require('./api/index'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// CORS
+// This allows client applications from other domains use the API Server
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
+//////////////////
+// API Queries
+//////////////////
+
+app.use('/api', api);
+
+// set static path [dist & api]
+app.use(express.static(path.join(__dirname, 'dist')));
+// app.use('/api', require('./api/index.js'));
+
+// set * to index.html
 app.get('*', function (req, res){
-    console.log('there is request comming');
     res.sendFile(path.join(__dirname, 'dist/index.html'),{
         new:{"n":1}
     });
 });
 
-const server = app.listen(8081, function(){
-    var host = server.address().address;
-    var port = server.address().port;
 
-    console.log('Running ... http://localhost%s', host, port);
-})
+//////////////////
+// Server Setup
+//////////////////
+
+app.set("env", process.env.NODE_ENV || "development");
+app.set("host", process.env.HOST || "0.0.0.0");
+app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"), function () {
+    console.log('\n' + '**********************************');
+    console.log('REST API listening on port ' + app.get("port"));
+    console.log('**********************************' + '\n');
+});
+
+
+////////////////////
+// Error Handlers
+////////////////////
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status( err.code || 500 )
+        .json({
+            status: 'error',
+            message: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+    .json({
+        status: 'error',
+        message: err.message
+    });
+});
+
+
+module.exports = app;
